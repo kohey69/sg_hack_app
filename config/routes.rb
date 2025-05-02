1129f7b2508a2ceb96d85e3328e1a9b6240b15ef
kuma <kuma@sonicgarden.jp>
2025-05-02 10:37:57 +0900

    home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'home#index'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
