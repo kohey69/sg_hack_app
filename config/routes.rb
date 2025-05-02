04a1eb057540c9490fe69a8230b2c46c8cd66b04
kuma <kuma@sonicgarden.jp>
2025-05-02 12:48:42 +0900

    food_sets controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   namespace :admins do
-    root 'home#index'
+    root 'food_sets#index'
+
+    resources :food_sets, only: %i[show new create edit update destroy]
   end
 
   if Rails.env.development?
