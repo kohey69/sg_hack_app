d31e17529fde55ed974451d044a4fb16b754b4b3
kuma <kuma@sonicgarden.jp>
2025-05-02 14:58:06 +0900

    Plan CRUD

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,7 +2,7 @@ class Plan < ApplicationRecord
   has_many :users, dependent: :restrict_with_error
   has_many :food_sets, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :description, presence: true
 
   scope :default_order, -> { order(:id) }
