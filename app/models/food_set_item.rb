391a7eabc37588b2c872381998aff19f7b7c0c9c
kuma <kuma@sonicgarden.jp>
2025-05-02 15:47:38 +0900

    プラン詳細画面作成

--- app/models/food_set_item.rb
+++ app/models/food_set_item.rb
@@ -6,4 +6,6 @@ class FoodSetItem < ApplicationRecord
 
   validates :food_id, uniqueness: { scope: :food_set_id }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :default_order, -> { order(position: :asc) }
 end
