8cf0a34a1351272b098309c22bca004d6b75342d
kuma <kuma@sonicgarden.jp>
2025-05-02 11:10:54 +0900

    devise user

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :recoverable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  validates :name, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+
   root 'home#index'
 
   if Rails.env.development?
